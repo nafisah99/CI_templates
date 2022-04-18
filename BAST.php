38d7d5948c1336f8730c7893760fcfc4c484dda2
nafisah99 <30711258+nafisah99@users.noreply.github.com>
2022-04-18 08:46:24 +0700

    Add files via upload

---
+++ BAST.php
@@ -0,0 +1,329 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <style>
+    @page {
+        size: A4;
+        margin-top: 0;
+        margin-right: 30px;
+    }
+
+    body {
+        font-size: 12px;
+        line-height: 24px;
+        font-family: "arial", "arial", Arial;
+        color: black;
+    }
+
+    header {
+        position: fixed;
+    }
+
+
+    #f_themes {
+        font-family: Arial;
+        border-collapse: collapse;
+        width: 100%;
+        margin: 0 0 0 0;
+        padding: 0 0 0 0;
+    }
+
+
+    #no_border {
+        font-family: Arial;
+        border-collapse: collapse;
+        width: 100%;
+        margin: 0 0 0 0;
+        padding: 0 0 0 0;
+    }
+
+
+    #no_border td,
+    #no_border th {
+        border: none;
+        padding: 8px;
+        margin: 0 0 0 0;
+        padding: 0 0 0 0;
+    }
+
+    #f_themes td,
+    #f_themes th {
+        border: 2px solid block;
+        padding-left: 8px;
+        color: black;
+        padding-right: 8px;
+        margin: 0 0 0 0;
+    }
+
+    .titles {
+        text-align: left;
+    }
+
+    #f_themes tr:nth-child(even) {
+        background-color: white;
+    }
+
+    #f_themes tr:hover {
+        background-color: #ddd;
+    }
+
+    #f_themes th,
+    .colour {
+        padding-top: 12px;
+        /* background-color: #e2efda;
+        color: #215e0c; */
+        margin: 0 0 0 0;
+        padding: 0 0 0 0;
+    }
+
+    #no_kwitansi {
+        text-align: left;
+        font-size: 12px;
+    }
+
+    #f_txt {
+        padding-left: 10px;
+    }
+
+    #p_txt {
+        padding-top: 2px;
+    }
+    </style>
+    <link href="<?php echo base_url('assets/bootstrap5/css/bootstrap.min.css');?>" rel="stylesheet">
+</head>
+
+<body>
+    <p style="text-align:right;">Halaman 01 / 02</p>
+    <main>
+        <table>
+            <tr>
+                <td colspan="6">
+                    <table>
+                        <tr>
+                            <td class="title" style="padding-top: 20px;">
+                                <img src="assets/picture/LAS.png" style="width:70px; max-width:110px;">
+                            </td>
+                            <td class="text-left" style="padding-left:10px;">
+                                <h3 class="font-weight-bold" style="margin-bottom:-1px;">PT. LUMBUNG AMANAH SOLUSI</h3>
+                                <span> Jl. Manunggal Pratama No. 8 <br>
+                                    085966656356</span>
+                            </td>
+                            <td class="title" style="padding-top:10px; padding-left: 180px;">
+                                <img src="assets/picture/siplah_blibli.jpg" style="width:150px;">
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <h4 class="text-center" style="text-align:center; margin-bottom:-1px;">BERITA ACARA SERAH TERIMA</h4>
+        <p class="text-center" style="text-align:center;margin-top:-1px;">Nomor: S10000854470</p>
+        <table>
+            <tbody>
+                <tr>
+                    <td colspan="8">Pada hari ini, Senin Tanggal 26 Bulan April Tahun 2021,sesuai dengan :</td>
+                </tr>
+                <tr>
+                    <td style="width:190px;">Nomor surat perjanjian</td>
+                    <td style="width:300px;">: S10000854470</td>
+                </tr>
+                <tr>
+                    <td style="width:190px;">Tanggal</td>
+                    <td style="width:300px;">: 26/04/21</td>
+                </tr>
+                <tr>
+                    <td style="width:190x;">Nama pekarjaan</td>
+                    <td style="width:300px;">: Kegiatan Jual-Beli melalui Mitra Siplah Bilbli.com</td>
+                </tr>
+                <tr>
+                    <td style="width:190px;">Tahun</td>
+                    <td style="width:300px;">: 2021</td>
+                </tr>
+            </tbody>
+        </table>
+        <table style="padding-top:10px;">
+            <tbody>
+                <tr>
+                    <td>Yang bertandatangan di bawah ini:</td>
+                </tr>
+                <tr>
+                    <td>1. Nama</td>
+                    <td style="width:300px;">: Ferry Febriawan</td>
+                </tr>
+                <tr>
+                    <td style="padding-left:12px; width:180px;"> Jabatan</td>
+                    <td style="width:300px;">: Direktur</td>
+                </tr>
+                <tr>
+                    <td style="padding-left:12px; width:180px;"> Nama Perusahaan</td>
+                    <td style="width:300px;">: PT. LUMBUNG AMANAH SOLUSI</td>
+                </tr>
+                <tr>
+                    <td style="padding-left:12px; width:180px;"> Alamat Perusahaan</td>
+                    <td style="width:300px;">: Jl. Manunggal Pratama No. 8</td>
+                </tr>
+                <tr>
+                    <td style="padding-left:12px; width:180px;"> No. Telepon</td>
+                    <td style="width:300px;">: 085966656356</td>
+                </tr>
+            </tbody>
+        </table>
+        <p style="margin-left:12px;">Sebagai pihak yang menyerahkan, selanjutnya disebut PIHAK PERTAMA</p>
+        <table>
+            <tbody>
+                <tr>
+                    <td>2. Nama</td>
+                    <td style="width:300px;">: NANANG KHOIRUDIN</td>
+                </tr>
+                <tr>
+                    <td style="padding-left:12px; width:180px;">NIP</td>
+                    <td style="width:300px;">: 198512282010011006</td>
+                </tr>
+                <tr>
+                    <td style="padding-left:12px; width:180px;">Jabatan</td>
+                    <td style="width:300px;">: Kepala Sekolah</td>
+                </tr>
+                <tr>
+                    <td style="padding-left:12px; width:180px;">Nama Sekolah</td>
+                    <td style="width:300px;">: SMP NEGERI 3 SATU ATAP PAGUYAMAN PANTAI</td>
+                </tr>
+                <tr>
+                    <td style="padding-left:12px; width:180px;">Alamat Sekolah</td>
+                    <td style="width:300px;">: Gorontalo</td>
+                </tr>
+                <tr>
+                    <td style="padding-left:12px; width:180px;">No. Telepon</td>
+                    <td style="width:300px;">: </td>
+                </tr>
+            </tbody>
+        </table>
+        <p style="margin-left:12px;">Sebagai pihak yang menerima, selanjutnya disebut PIHAK KEDUA</p>
+        <p style="margin-left:2px; margin-right:9px; text-align: justify;">PIHAK PERTAMA menyerahkan hasil pekerjaan
+            pengiriman barang atas kegiatan jual-beli melalui Mitra Siplah Blibli.com kepada PIHAK KEDUA, dan PIHAK
+            KEDUA
+            telah menerima hasil pekerjaan tersebut dalam jumlah yang lengkap dan kondisi yang baik sesuai dengan
+            rincian
+            berikut :</p>
+        <table id="f_themes">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama Barang / Jasa</th>
+                    <th>Jumlah Dipesan</th>
+                    <th>Harga Satuan (Rp)</th>
+                    <th>Jumlah Diterima</th>
+                    <th>Jumlah Rusak</th>
+                    <th>Jumlah Sesuai</th>
+                    <th>Total (Rp)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>
+                        <!-- Title -->
+                        LEMARI BUKU 2 SISI / PERPUSTAKAAN <br>
+                        <!-- Category -->
+                        Barang Umum > Kebutuhan sekolah > Kebutuhan Kantor > Set Meja & Kursi <br>
+                        <!-- PPN -->
+                        Barang Tidak Kena PPN
+                    </td>
+                    <td>1</td>
+                    <td>2.160.000</td>
+                    <td>1</td>
+                    <td>0</td>
+                    <td>1</td>
+                    <td>2.160.000</td>
+                </tr>
+            </tbody>
+        </table>
+        <table id="f_themes">
+            <thead>
+                <tr>
+                    <td colspan="9" style="border-top:none;">Penyetoran Pajak : Disetor oleh Satdik</td>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="text-align:right; border-right:none; border-bottom:none;" colspan="8"><b>Subtotal
+                            (Rp)</b></td>
+                    <td style="text-align:right; border-left:none; border-bottom:none; width:100px;">2.160.000</td>
+                </tr>
+                <tr>
+                    <td style="text-align:right; border-right:none;border-bottom:none; border-top:none;" colspan="8">
+                        <b>Total sebelum PPN (Rp)</b>
+                    </td>
+                    <td style="text-align:right; border-left:none; border-bottom:none; border-top:none;">2.160.000</td>
+                </tr>
+                <tr>
+                    <td style="text-align:right; border-right:none; border-top:none; border-bottom:none;" colspan="8">
+                        <b>PPN (Rp)</b>
+                    </td>
+                    <td style="text-align:right; border-left:none; border-top:none; border-bottom:none;">0</td>
+                </tr>
+                <tr>
+                    <td style="text-align:right; border-right:none; border-top:none; border-bottom:none;" colspan="8">
+                        <b>Biaya Kirim (Rp)</b>
+                    </td>
+                    <td style="text-align:right; border-left:none; border-top:none; border-bottom:none;">0</td>
+                </tr>
+                <tr>
+                    <td style="text-align:right; border-right:none;" colspan="8">
+                        <b>Biaya Kirim (Rp)</b>
+                    </td>
+                    <td style="text-align:right; border-left:none;">2.160.000</td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <p style="margin-left:2px; margin-right:9px; text-align: justify;">Berita Acara Serah Terima ini berfungsi
+            sebagai
+            bukti serah terima hasil pekerjaan kepada PIHAK KEDUA, untuk selanjutnya dicatat pada buku penerimaan barang
+            satuan pendidikan.</p>
+        <p style="margin-left:2px; margin-right:9px; text-align: justify;">Demikian Berita Acara Serah Terima ini dibuat
+            dengan sebenarnya untuk dipergunakan sebagaimana seharusnya.</p>
+        <br>
+        <br>
+        <table>
+            <tbody>
+                <tr>
+                    <td style="text-align:center;padding-left:100px; width:200px">Pihak Kedua</td>
+                    <td style="text-align:center;padding-left:100px; width:200px">Pihak Pertama</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;padding-left:100px; padding-top:30px; width:200px">TTD</td>
+                    <td style="text-align:center;padding-left:100px; padding-top:30px; width:200px">TTD</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;padding-left:110px; padding-top:30px; width:200px">NANANG KHOIRUDIN
+                    </td>
+                    <td style="text-align:center;padding-left:100px; padding-top:30px; width:200px">Ferry Febriawan</td>
+                </tr>
+                <tr>
+                    <td style="text-align:center;padding-left:110px;width:200px">NIP: 198512282010011006
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <p style="text-align:center;">Dokumen ini dapat diunduh melalui tautan:</p>
+        <p style="text-align:center; color:blue;">https://siplah.blibli.com/backend/order-doc/S10000854470/digital-bast
+        </p>
+        <table id="f_themes">
+            <tbody>
+                <tr>
+                    <td colspan="8" style="text-align:center;font-size:14px;">Lampiran</td>
+                </tr>
+            </tbody>
+        </table>
+    </main>
+    <script src=" <?php echo base_url('assets/bootstrap5/js/jquery.min.js');?>">
+    </script>
+    <script src="<?php echo base_url('assets/bootstrap-5.1.3-dist/js/bootstrap.min.js');?>">
+    </script>
+</body>
+
+</html>
---
+++ SPK.php
@@ -0,0 +1,218 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <style>
+    @page {
+        size: A4;
+        margin-top: 0;
+        margin-right: 30px;
+    }
+
+    body {
+        font-size: 12px;
+        line-height: 24px;
+        font-family: "arial", "arial", Arial;
+        color: black;
+    }
+
+    #f_themes {
+        font-family: Arial;
+        border-collapse: collapse;
+        width: 100%;
+        margin: 0 0 0 0;
+        padding: 0 0 0 0;
+    }
+
+
+    #no_border {
+        font-family: Arial;
+        border-collapse: collapse;
+        width: 100%;
+        margin: 0 0 0 0;
+        padding: 0 0 0 0;
+    }
+
+
+    #no_border td,
+    #no_border th {
+        border: none;
+        padding: 8px;
+        margin: 0 0 0 0;
+        padding: 0 0 0 0;
+    }
+
+    #f_themes td,
+    #f_themes th {
+        border: 2px solid block;
+        padding-left: 8px;
+        padding-right: 8px;
+        margin: 0 0 0 0;
+    }
+
+    .titles {
+        text-align: left;
+    }
+
+    #f_themes tr:nth-child(even) {
+        background-color: white;
+    }
+
+    #f_themes tr:hover {
+        background-color: #ddd;
+    }
+
+    #f_themes th,
+    .colour {
+        padding-top: 12px;
+        /* background-color: #e2efda;
+        color: #215e0c; */
+        margin: 0 0 0 0;
+        padding: 0 0 0 0;
+    }
+
+    #no_kwitansi {
+        text-align: left;
+        font-size: 12px;
+    }
+
+    #f_txt {
+        padding-left: 10px;
+    }
+
+    #p_txt {
+        padding-top: 2px;
+    }
+    </style>
+    <link href="<?php echo base_url('assets/bootstrap5/css/bootstrap.min.css');?>" rel="stylesheet">
+</head>
+
+<body>
+    <p style="text-align:right;">Halaman 01 / 01</p>
+    <main>
+        <img style="width:200px; height:100px;" src="assets/picture/siplah_black.JPG" alt="" />
+        <br>
+        <table id="f_themes">
+            <thead>
+                <tr>
+                    <td colspan="3" rowspan="2">Surat Perintah Kerja SPK</td>
+                    <td colspan="3">Nama Satuan Pendidikan : SMP NEGERI 3 SATU ATAP PAGUYAMAN PANTAI</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="border-bottom:none;">Nomor SPK : S10000854470</td>
+                </tr>
+                <tr>
+                    <td colspan="3" rowspan="3">Paket Pekerjaan: <br>
+                        Kegiatan jual beli melalui mitra SIPLah Blibli.com</td>
+                    <td colspan="3" style="border-top:none;">Tanggal SPK : 26/04/21</td>
+                </tr>
+                <tr>
+                    <td colspan="3">Tanggal Negosiasi : </td>
+                </tr>
+                <tr>
+                    <td colspan="3">SPK ini mulai berlaku efektif terhitung sejak tanggal ditetapkan dan penyelesaian
+                        keseluruhan pekerjaan sebagaimana dimaksud dalam SPK ini.</td>
+                </tr>
+                <tr>
+                    <td colspan="6">
+                        Waktu Pelaksanaan Pekerjaan : 26/04/21 <br>
+                        Waktu Pemrosesan Pekerjaan : 10/05/21 <br>
+                        Waktu Penyelesaian Pekerjaan : 28/05/21 <br>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="6">Penyetoran Pajak : Disetor oleh Satdik</td>
+                </tr>
+                <tr>
+                    <td colspan="6" style="text-align:center">Rincian Pekerjaan</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>No.</td>
+                    <td>Uraian Barang/Jasa</td>
+                    <td>Jumlah (QTY)</td>
+                    <td>Satuan Ukuran</td>
+                    <td>Harga Satuan</td>
+                    <td>Total Harga</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>LEMARI BUKU 2 SISI / PERPUSTAKAAN <br> Barang Tidak Kena PPN</td>
+                    <td>1</td>
+                    <td>Unit</td>
+                    <td>Rp2.160.000</td>
+                    <td>Rp2.160.000</td>
+                </tr>
+                <tr>
+                    <td style="text-align:right; border-right:none; border-bottom:none;" colspan="5"><b>Subtotal
+                            (Rp)</b></td>
+                    <td style="text-align:right; border-left:none; border-bottom:none;">2.160.000</td>
+                </tr>
+                <tr>
+                    <td style="text-align:right; border-right:none;border-bottom:none; border-top:none;" colspan="5">
+                        <b>Total sebelum
+                            PPN (Rp)</b>
+                    </td>
+                    <td style="text-align:right; border-left:none; border-bottom:none; border-top:none;">2.160.000</td>
+                </tr>
+                <tr>
+                    <td style="text-align:right; border-right:none; border-top:none; border-bottom:none;" colspan="5">
+                        <b>Biaya Kirim
+                            (Rp)</b>
+                    </td>
+                    <td style="text-align:right; border-left:none; border-top:none; border-bottom:none;">0</td>
+                </tr>
+                <tr>
+                    <td style="text-align:right; border-right:none; border-top:none; border-bottom:none;" colspan="5">
+                        <b>Asuransi
+                            (Rp)</b>
+                    </td>
+                    <td style="text-align:right; border-left:none; border-top:none; border-bottom:none;">--</td>
+                </tr>
+                <tr>
+                    <td style="text-align:right; border-right:none; border-top:none;" colspan="5"><b>Total Pembayaran
+                            (Rp)</b></td>
+                    <td style="text-align:right; border-left:none; border-top:none;">2.160.000</td>
+                </tr>
+                <tr>
+                    <td colspan="6">Terbilang : Dua juta seratus enam puluh ribu rupiah</td>
+                </tr>
+                <tr>
+                    <td colspan="6" style="border-bottom:none; padding-bottom:50px">INSTRUKSI KE PENYEDIA : <br>
+                        Penagihan hanya dapat dilakukan setelah penyelesaian pekerjaan yang diperintahkan dalam SPK ini
+                        dan dibuktikan dengan Berita Acara
+                        Serah Terima</td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="3"
+                        style="text-align:left; padding-bottom:70px; border-right:none; border-top:none; border-bottom:none;">
+                        Penyedia</td>
+                    <td colspan="3"
+                        style="padding-left:180px; padding-bottom:70px; text-align:left; border-left:none;  border-bottom:none; border-top:none;">
+                        Kab.
+                        Boalemo, Kamis 24
+                        Februari 2022 <br> Pelaksana</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="text-align:left; border-right:none; border-top:none;">
+                        Ferry Febriawan <br>
+                        PT. LUMBUNG AMANAH SOLUSI
+                    </td>
+                    <td colspan="3" style="padding-left:180px; text-align:left; border-left:none; border-top:none;">
+                        NANANG KHOIRUDIN <br>
+                        NIP: 198512282010011006</td>
+                </tr>
+            </tfoot>
+        </table>
+    </main>
+    <script src=" <?php echo base_url('assets/bootstrap5/js/jquery.min.js');?>">
+    </script>
+    <script src="<?php echo base_url('assets/bootstrap-5.1.3-dist/js/bootstrap.min.js');?>">
+    </script>
+</body>
+
+</html>
